eb758ee17393a5038d0bf947c9b6a3d8d88313c1
imgopz <rrgkmech@gmail.com>
2024-08-03 11:20:22 +0530

    basic app

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,31 @@
-console.log(
-  "Hello World!"
-);
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+// GET 
+app.get("/hello", (req, res, next)=>{
+  return res.send("Hello");
+});
+
+// PUT
+app.put("/hello", (req, res, next)=>{
+  console.log(req.body.name);
+  return res.send("Hello");
+});
+
+// POST
+app.post("/hello", (req, res, next)=>{
+  console.log(req.body.name);
+  return res.send("Hello");
+});
+
+// DELETE
+app.delete("/user/:id", (req, res, next)=>{
+  console.log(req.params.id);
+  console.log(req.body.name);
+  return res.send("Hello");
+});
+
+app.listen(5000, ()=>console.log("Server started..."));
