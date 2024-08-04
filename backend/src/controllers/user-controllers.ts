2035d30bf39337dac7a925ac2092d7a8102b7d08
imgopz <rrgkmech@gmail.com>
2024-08-04 21:52:30 +0530

    jwt auth token create, clear, cookie

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 import User from "../models/User.js";
 import { hash, compare } from "bcrypt";
+import { createToken } from "../utils/token-manager.js";
+import { COOKIE_NAME } from "../utils/constants.js";
 
 export const getAllUsers = async (
   req: Request,
@@ -36,6 +38,25 @@ export const userSignup = async (
 
     await user.save();
 
+    // create token and store cookie
+    res.clearCookie(COOKIE_NAME, {
+      httpOnly: true,
+      domain: "localhost",
+      signed: true,
+      path: "/",
+    });
+
+    const token = createToken(user._id.toString(), user.email, "7d");
+    const expires = new Date();
+    expires.setDate(expires.getDate() + 7);
+    res.cookie(COOKIE_NAME, token, {
+      path: "/",
+      domain: "localhost",
+      expires,
+      httpOnly: true,
+      signed: true,
+    });
+
     return res.status(201).json({ message: "OK", id: user._id.toString() });
   } catch (error) {
     console.log(error);
@@ -62,6 +83,27 @@ export const userlogin = async (
     if (!isPasswordCorrect) {
       return res.status(401).send("User or password is incorrect!");
     }
+
+    res.clearCookie(COOKIE_NAME, {
+      httpOnly: true,
+      domain: "localhost",
+      signed: true,
+      path: "/",
+    });
+
+    const token = createToken(user._id.toString(), user.email, "7d");
+
+    const expires = new Date();
+
+    expires.setDate(expires.getDate() + 7);
+
+    res.cookie(COOKIE_NAME, token, {
+      path: "/",
+      domain: "localhost",
+      expires,
+      httpOnly: true,
+      signed: true,
+    });
 
     return res.status(200).json({ message: "OK", id: user._id.toString() });
   } catch (error) {
