6a0d084c224667f70797bc856d170ff5308eb111
imgopz <rrgkmech@gmail.com>
2024-08-05 14:45:25 +0530

    auth provider and navigation link

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { AppBar, Toolbar } from "@mui/material";
 import Logo from "./shared/Logo";
+import { useAuth } from "../context/AuthContext";
+import NavigationLink from "./shared/NavigationLink";
 
 const Header = () => {
+  const auth = useAuth();
+
   return (
     <AppBar
       sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}
     >
       <Toolbar sx={{ display: "flex" }}>
         <Logo />
+        <div>{auth?.isLoggedIn ? <></> : <></>}</div>
       </Toolbar>
     </AppBar>
   );
