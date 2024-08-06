a0a210fffccfec4a6f91ebbe20c888a40ca09c50
imgopz <rrgkmech@gmail.com>
2024-08-06 09:58:07 +0530

    adding chat backend logic

--- backend/src/routes/chat-routes.ts
+++ backend/src/routes/chat-routes.ts
@@ -1,5 +1,16 @@
 import { Router } from "express";
+import { verifyToken } from "../utils/token-manager.js";
+import { chatCompletionValidator, validate } from "../utils/validator.js";
+import { generateChatCompletion } from "../controllers/chat-controllers.js";
 
+// this route should be protected, only authenicated and authorized user should see their own chat
 const chatRoutes = Router();
+
+chatRoutes.post(
+  "/new",
+  validate(chatCompletionValidator),
+  verifyToken,
+  generateChatCompletion
+);
 
 export default chatRoutes;
